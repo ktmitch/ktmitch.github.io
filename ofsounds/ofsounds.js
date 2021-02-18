cfbad42863dda45a0927bb0a16116b3382fb5212
ktmitch <61105031+ktmitch@users.noreply.github.com>
2021-02-18 16:37:54 -0500

    Updating Portfolio Website with Destiny Project
    Updating Portfolio website and slight edit to personal Soundboard

--- ofsounds/ofsounds.js
+++ ofsounds/ofsounds.js
@@ -1,27 +1,37 @@
-const one = document.getElementById("kevpanting");
-const two = document.getElementById("adub");
-const three = document.getElementById("hurt");
-const four = document.getElementById("djsing");
-const five = document.getElementById("yeah");
+const one = document.getElementById("kevpanting"); // Kevin Panting
+const two = document.getElementById("adub"); // Kevin singing A Dub
+const three = document.getElementById("hurt"); // Minecraft Hurt
+const four = document.getElementById("thanksgiving"); // DJ Singing
+const five = document.getElementById("yeah"); // Bryan saying Yeah
 
+let headingcolor = document.getElementById("title");
 // Play, Can add a Stop for other buttons
 
 function playAudio() {
+    headingcolor.style.color = "#0099ff";
     one.play();
+    console.log(document.getElementById("kevpanting").duration)
+    lights();
 }
+
 function playAudiotwo() {
+    headingcolor.style.color = "#0099ff";
     two.play();
 }
+
 function playAudiothree() {
     three.play();
+    headingcolor.style.color = "#00cc99";
 }
 
 function playAudiofour() {
     four.play();
+    headingcolor.style.color = "#f3aa21";
 }
 
 function playAudiofive() {
     five.play();
+    headingcolor.style.color = "#c6d400";
 }
 
 // Pause
@@ -42,7 +52,28 @@ function stopAudio() {
     three.pause();
     document.getElementById("hurt").currentTime = 0  
     four.pause();
-    document.getElementById("djsing").currentTime = 0
+    document.getElementById("thanksgiving").currentTime = 0
     five.pause();
     document.getElementById("yeah").currentTime = 0  
   } 
+
+  function darkmode() {
+    if (document.getElementById("darkmode").innerHTML = "Toggle Dark Mode") {
+        document.bgColor = "black"
+        document.getElementById("darkmode").innerHTML = "Toggle Light Mode"
+    }
+    else if (document.getElementById("darkmode").innerHTML = "Toggle Light Mode") {
+      document.bgColor = "white";
+      document.getElementById("darkmode").innerHTML = "Toggle Dark Mode"
+    }
+    else {
+        document.bgColor = "white";
+    }
+  }
+
+  function lights() {
+        document.bgColor = "black"
+
+
+
+  }
